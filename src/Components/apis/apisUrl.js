e97613e0c0dd3efa3c3ba555946cf0d4b434618b
Hanzala Khattak <muhammadhanzalahkhan1@gmail.com>
2025-08-26 15:53:21 +0500

    Remove apisUrl.js and endpoints.js from git tracking

--- src/Components/apis/apisUrl.js
+++
@@ -1,6 +0,0 @@
-export const API_User = 'http://localhost:3000/api/v0/users';
-
-export const API_Doctor = 'http://localhost:3000/api/v0/doctors';
-
-
-export const API_HospitalAdmin = 'http://localhost:3000/api/v0/hospitalAdmin';
--- src/Components/apis/endpoints.js
+++
@@ -1,6 +0,0 @@
-export const Add_User = '/postuser' //add new user
-export const Login_User = '/UserLogin' //login user
-
-export const Register_Doctor = '/registerDoctor' //register doctor
-
-export const Register_HospitalAdmin = '/registerHospitalAdmin' // register Hospital Admin
